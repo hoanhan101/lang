9f7a978cf665b7ab65565dd9324ff195d8aaaa64
Hoanh An <hoanhan@bennington.edu>
2019-12-04 23:33:22 -0500

    add tests and comment

--- swift/revstring.swift
+++ swift/revstring.swift
@@ -1,5 +1,6 @@
 #!/usr/bin/swift
 
+// reverseString reverses a list of string in place.
 func reverseString(input: [String]) -> [String] {
     var s = input
     var start = 0
@@ -17,4 +18,27 @@ func reverseString(input: [String]) -> [String] {
     return s
 }
 
-print(reverseString(input: ["h", "e", "l", "l", "o"]))
+func assertEqual(s1: [String], s2: [String]) {
+    if s1 != s2 {
+        print(s1, "is not equal to", s2)
+    } else {
+        print("passed")
+    }
+}
+
+struct test {
+    var input: [String]
+    var expected: [String]
+}
+
+var tests: [test] = [
+    test(input: [], expected: []),
+    test(input: ["h"], expected: ["h"]),
+    test(input: ["h", "e"], expected: ["e", "h"]),
+    test(input: ["h", "e", "l"], expected: ["l", "e", "h"]),
+    test(input: ["h", "e", "l", "l", "o"], expected: ["o", "l", "l", "e", "h"])
+]
+
+for t in tests {
+    assertEqual(s1: t.expected, s2: reverseString(input: t.input))
+}
