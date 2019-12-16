4c3f9141928fed9005ba64c83dcc007b7682dbbd
Hoanh An <hoanhan@bennington.edu>
2019-12-16 12:51:07 -0500

    add prompt

--- swift/substring.swift
+++ swift/substring.swift
@@ -1,5 +1,21 @@
 #!/usr/bin/swift
 
+/*
+- Given a string, find the length of the longest substring in it with no more
+  than k distinct characters.
+
+Example:
+- Input: string="araaci", k=1
+  Output: 2
+  Explanation: Longest substring with no more than 1 distinct characters is "aa".
+- Input: string="araaci", k=2
+  Output: 4
+  Explanation: Longest substring with no more than 2 distinct characters is "araa".
+- Input: string="araaci", k=3
+  Output: 5
+  Explanation: Longest substring with no more than 3 distinct characters is "araac".
+*/
+
 func longestSubstring(s: String, k: Int) -> Int {
     var maxLength: Int = 0
     var start: Int = 0
